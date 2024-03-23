999de69d901f73d38ecd93918eaadf96c695e717
hagustin2 <hagustin2@nait.ca>
2024-03-22 21:06:06 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+/*
+Steps 1-3 READ THE PDF!
+*/
+(function () {
+  let videoGameForm = document.querySelector('#video-game-form');
+  let allGameList = document.querySelector('.all-games');
+  let allGameListItems = document.querySelectorAll('.all-games li');
+  let myGameList = document.querySelector('.my-favourite-games');
+  let myGames = [];
+
+  function filterGames(platform) {
+    allGameListItems.forEach(item => {
+      if (item.innerText.toLowerCase().includes(platform.toLowerCase())) {
+        item.classList.remove('hidden-item');
+      } else {
+        item.classList.add('hidden-item');
+      }
+    });
+  }
+
+  // event listener for step 1
+  videoGameForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let platform = event.target.elements['platform-family'].value.toLowerCase();
+    filterGames(platform);
+  });
+
+  // event listener for step 2
+  /* HTML for step 2 to add to the list
+  <li class="list-group-item">VIDEO GAME NAME HERE</li>
+  */
+  allGameList.addEventListener('click', (event) => {
+    let game = event.target.innerText;
+    addToFavouriteGames(game);
+  });
+
+  function addToFavouriteGames(game) {
+    myGames.push(game); 
+    renderFavouriteList(); 
+  }
+
+  function renderFavouriteList() {
+    myGameList.innerHTML = ''; 
+    myGames.forEach(game => {
+      myGameList.innerHTML += `<li class="list-group-item">${game}</li>`; 
+    });
+  }
+
+
+  // event listener for step 3
+  function removeFromFavouriteGames(game) {
+    let index = myGames.indexOf(game);
+    if (index !== -1) {
+      myGames.splice(index, 1);
+      renderFavouriteList();
+    }
+  }
+
+  myGameList.addEventListener('click', (event) => {
+    let favGame = event.target.innerText;
+    removeFromFavouriteGames(favGame);
+  });
+
+})();
